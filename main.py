237035485931c40f0270ad1c6f62d42bd20d4df0
bamblebam <burhanizrangwala@gmail.com>
2021-01-07 21:29:07 +0530

    df merge

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # %%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from matplotlib import rcParams
+rcParams['figure.figsize'] = 22, 10
+RANDOM_SEED = 42
+# %%
+exercise_df = pd.read_csv('datasets/exercise.csv')
+calories_df = pd.read_csv('datasets/calories.csv')
+df = pd.concat([exercise_df, calories_df], axis=1)
+df.head()
+# %%
+df.drop(['User_ID'], axis=1, inplace=True)
+df.head()
+# %%
